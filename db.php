a65fbdab3a49fb5f6f098d787de3277a7b99fb08
xeniya0410 <kseniatazelova@gmail.com>
2025-12-03 10:18:55 +0500

    db.php

--- db.php
+++ db.php
@@ -4,14 +4,15 @@ $db = 'practice_db';
 $user = 'root';
 $pass = 'root';
 $charset = 'utf8mb4';
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset"; //строка подключения - DSN (Data Source Name) необход для pdo
 $options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //любые ошибки PDO будут вызывать исключение
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //fetch() будет возвращать строки в виде ассоциативного массива
 ];
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
+    $pdo = new PDO($dsn, $user, $pass, $options); //если все прошло успешно, в переменной $pdo появится рабочее соединение с базой данных
 } catch (\PDOException $e) {
     die("Ошибка подключения: " . $e->getMessage());
 }
+
 ?>
